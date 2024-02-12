16b0eaa5ea1052704dd3d75fab895019f0eeeab0
viratyogi06 <virat.yogi06@gmail.com>
2024-02-12 19:11:27 +0530

    added post event

--- Node/postEvent.mjs
+++ Node/postEvent.mjs
@@ -4,11 +4,13 @@ const server = http.createServer((req,res)=>{
     if(req.method === 'POST'){
         let body = ''
 
+        // listens to the data event everytime and adds to the body
         req.on('data',(chunk)=>{
             
             body+=chunk
         })
 
+        // listens to the end event, that triggers when all data events are received  
         req.on('end',()=>{
             console.log('Data Recieved - ', body)
         })
